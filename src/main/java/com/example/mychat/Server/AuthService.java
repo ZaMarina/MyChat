8d79d706bac6f1542048ab867c3148bb482ce9a1
Marina <marinamingaleva92@gmail.com>
2022-06-18 22:33:39 +0700

    master

--- src/main/java/com/example/mychat/Server/AuthService.java
+++
@@ -1,8 +0,0 @@
-package com.example.mychat.Server;
-
-import java.io.Closeable;
-
-//класс который хранит логины и пароли
-public interface AuthService extends Closeable{
-    String getNickByLoginAndPassword(String login, String password);
-}
--- src/main/java/com/example/mychat/Server/ServerLauncher.java
+++
@@ -1,7 +0,0 @@
-package com.example.mychat.Server;
-
-public class ServerLauncher {
-    public static void main(String[] args) {
-        new ChatServer().run();
-    }
-}
